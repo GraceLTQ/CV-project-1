ee6382b80f982a6d7785ba727051ec909b058dd5
Evelyn Si <evelyna.217@gmail.com>
2024-03-18 15:04:31 -0400

    Implemented photometric_stereo function in photometric_stereo.py

--- photometric_stereo.py
+++ photometric_stereo.py
@@ -2,10 +2,6 @@ import numpy as np
 ##======================== No additional imports allowed ====================================##
 
 
-
-
-
-
 def photometric_stereo_singlechannel(I, L):
     #L is 3 x k
     #I is k x n
@@ -16,6 +12,20 @@ def photometric_stereo_singlechannel(I, L):
     normals = G/(albedo.reshape((1,-1)) + (albedo==0).astype(float).reshape((1,-1)))
     return albedo, normals
 
+
+def normalize(normals):
+    '''
+    Normalizes the vectors in normals
+    Input:
+        normals: H x W x 3 array of vectors
+    
+    '''
+    h,w,_ = normals.shape
+    for r in range(h):
+        for c in range(w):
+            norm = np.linalg.norm(normals[r,c,:])
+            normals[r,c,:] = normals[r,c,:]/norm
+    
 
 def photometric_stereo(images, lights):
     '''
@@ -34,7 +44,28 @@ def photometric_stereo(images, lights):
         and renormalize so that they are unit norm
 
     '''
-    pass
+    num_lights = len(images) #number of lights, which is the same as number of images
+    h,w,_ = images[0].shape   
+    num_pixels=h*w #total number of pixels in an image
+
+    albedo = np.zeros((h,w,3))
+    normals = np.zeros((3,num_pixels))
+    
+    for c in range(3): #for each channel
+        I = np.zeros((num_lights,num_pixels))
+        for row in range(h):
+            for col in range(w):
+                for img in range(num_lights):
+                    I[img,row*w+col] = images[img][row,col,c]
+        a,nor = photometric_stereo_singlechannel(I,lights)
+        albedo[:,:,c] = a.reshape((h,w))
+        normals = normals + nor
+
+    normals = normals/3
+    normals = np.reshape(normals.T,(h,w,3))
+    normalize(normals)
+ 
+    return albedo,normals
 
 
 
